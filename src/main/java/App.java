57bb5d6961564c968e5c1b24ac8926a5eaadf883
Abel <abelberhane011@gmail.com>
2019-11-16 20:09:36 +0300

    [fixes #169797071] index template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,5 +18,17 @@ public class App {
         Hero.setUpNewHero1();
         Hero.setUpNewHero2();
 
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/hero",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> hero = Hero.getAllInstances();
+            model.put("hero",hero);
+            return new ModelAndView(model, "hero.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
