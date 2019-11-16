ea2d921dd6e87e491b459024c8126b003683da7e
Abel <abelberhane011@gmail.com>
2019-11-16 18:42:51 +0300

    [fixes #169776278] adds hero class and tests

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,83 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Hero {
+
+    private String heroName;
+    private int heroAge;
+    private String superPower;
+    private String weakness;
+    private int id;
+    private static ArrayList<Hero> instances = new ArrayList<>();
+    private int mAttack;
+    private int mDefense;
+    private int mHealth;
+
+
+    public Hero(String heroName, int heroAge, String superPower, String weakness) {
+        this.heroName = heroName;
+        this.heroAge = heroAge;
+        this.superPower = superPower;
+        this.weakness = weakness;
+        instances.add(this);
+        this.id = instances.size();
+        mAttack = 25;
+        mDefense = 15;
+        mHealth = 100;
+
+    }
+
+    public String getHeroName() {
+        return this.heroName;
+    }
+    public int getHeroAge(){
+        return this.heroAge;
+    }
+    public String getSuperPower(){
+        return this.superPower;
+    }
+    public String getWeakness(){
+        return this.weakness;
+    }
+    public static  ArrayList<Hero> getAllInstances(){
+        return instances;
+    }
+    public static void clearAllHeroes(){
+        instances.clear();
+    }
+    public int getId(){
+        return id;
+    }
+
+    public void setHeroName(String heroName){
+        this.heroName = heroName;
+    }
+    public void setHeroAge(int heroAge){
+        this.heroAge = heroAge;
+    }
+    public void setSuperPower(String superPower){
+        this.superPower = superPower;
+    }
+    public void setWeakness(String weakness){
+        this.weakness = weakness;
+    }
+
+    public int getmAttack() {
+        return mAttack;
+    }
+
+    public int getmDefense() {
+        return mDefense;
+    }
+
+    public int getmHealth() {
+        return mHealth;
+    }
+    public void attacks(Hero opponent){
+        opponent.mHealth -= mAttack;
+    }
+    public void defends(){
+        mHealth += 6;
+    }
 }
--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,42 @@
-package models;
-
-public class Squad {
-}
+//package models;
+//import models.Hero;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Squad {
+//
+//    private List<Hero> mSquadMembers = new ArrayList<>();
+//    private static ArrayList<Hero> heroInstances = new ArrayList<>();
+//    private String mSquadName;
+//    private int mMaxSize;
+//
+//    public Squad(String mSquadName, int mMaxSize) {
+//        this.mSquadName = mSquadName;
+//        this.mMaxSize = heroInstances.size();
+//    }
+//
+//    public List<Hero> getSquadMembers() {
+//        return mSquadMembers;
+//    }
+//
+//    public String getSquadName() {
+//        return mSquadName;
+//    }
+//
+//    public static ArrayList<Hero> getMaxSize(){
+//        return heroInstances;
+//    }
+//
+//    public String addHero(Hero newHero) {
+//        if (mMaxSize <= 3) {
+//            mSquadMembers.add(newHero);
+//            mMaxSize++;
+//            return "Hero successfully added!";
+//        } else {
+//            return "Squad is full. Create a new Squad.";
+//        }
+//    }
+//}
+//
+//
